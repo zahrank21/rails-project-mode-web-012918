d3ea1792fae45235de9d9457c96034ecedc5691b
github username <github email address>
2018-03-07 13:23:06 -0500

    made search bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
 
   def welcome
   end
+
+  def search
+    if Show.exists?(title: params[:search])
+      show = Show.find_by(title: params[:search])
+      redirect_to show_path(show)
+    elsif Actor.exists?(name: params[:search])
+      actor = Actor.find_by(name: params[:search])
+      redirect_to actor_path(actor)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/login', to: "session#new", as: "login"
   get '/users/:id/follow', to: "users#follow"
   get '/users/:id/unfollow', to: "users#unfollow"
+  get '/search', to: "application#search", as: "search"
   # post '/users/:id/following', to: "users#following"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
